fd8d8f70b9627c20d7ff958faa65fefaea74390b
Jack C <84105527+jackstaa@users.noreply.github.com>
2025-04-30 17:08:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -135,10 +135,10 @@ function preload() {
 
   // Define items (positioned in mid-air)
   items = [
-    { stageIndex: 0, x: 300, y: 200, width: 32, height: 32, image: itemImages[0], collected: false },
-    { stageIndex: 1, x: 400, y: 300, width: 32, height: 32, image: itemImages[1], collected: false },
+    { stageIndex: 3, x: 300, y: 200, width: 32, height: 32, image: itemImages[0], collected: false },
+    { stageIndex: 4, x: 400, y: 300, width: 32, height: 32, image: itemImages[1], collected: false },
     { stageIndex: 2, x: 500, y: 400, width: 32, height: 32, image: itemImages[2], collected: false },
-    { stageIndex: 3, x: 600, y: 500, width: 32, height: 32, image: itemImages[3], collected: false }
+    { stageIndex: 5, x: 600, y: 500, width: 32, height: 32, image: itemImages[3], collected: false }
   ];
 }
 
